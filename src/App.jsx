ff8e564580b20989bb062a3b8ff009250b9111d8
avanycmurali <avanycmuralidharan@gmail.com>
2026-07-13 10:23:49 +0530

    view donors created

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import NavigationBar from './components/NavigationBar'
 import AddDonor from './components/AddDonor'
+import ViewDonor from './components/ViewDonor'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
     <AddDonor/>
+    <ViewDonor/>
     </>
   )
 }
